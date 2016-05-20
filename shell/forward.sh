b74c96656ff36e8e5b1cf36149c5720441190336
ma yingkui <maeikei@gmail.com>
2016-05-21 00:18:16 +0900

    Update forward.sh

--- shell/forward.sh
+++ shell/forward.sh
@@ -2,4 +2,10 @@
 stty -F /dev/ttyUSB0 9600
 echo "a.s:10000" >/dev/ttyUSB0
 echo "a.t:fwd" >/dev/ttyUSB0
+echo "b.s:10000" >/dev/ttyUSB0
+echo "b.t:fwd" >/dev/ttyUSB0
+echo "c.s:10000" >/dev/ttyUSB0
+echo "c.t:fwd" >/dev/ttyUSB0
+echo "d.s:10000" >/dev/ttyUSB0
+echo "d.t:fwd" >/dev/ttyUSB0
 echo "run" >/dev/ttyUSB0
