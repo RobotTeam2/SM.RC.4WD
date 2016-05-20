62a1b087f894f0431c4cdbe1578dd90f574fae9f
Ma Eikei <maeikei@gmail.com>
2016-05-21 02:07:01 +0900

    add commands

---
+++ shell/left.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "a.s:10000" > /dev/ttyUSB0
+echo "a.t:fwd" > /dev/ttyUSB0
+echo "b.s:10000" > /dev/ttyUSB0
+echo "b.t:rev" > /dev/ttyUSB0
+echo "c.s:10000" > /dev/ttyUSB0
+echo "c.t:fwd" > /dev/ttyUSB0
+echo "d.s:10000" > /dev/ttyUSB0
+echo "d.t:rev" > /dev/ttyUSB0
+echo "run" > /dev/ttyUSB0
+
