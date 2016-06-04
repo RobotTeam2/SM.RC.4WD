5784cd3a53c5cf28a3d7f06f613e9e1d33428fae
ma yingkui <maeikei@gmail.com>
2016-06-04 21:36:01 +0900

    Update subdrift.forward.sh

--- shell/subdrift.forward.sh
+++ shell/subdrift.forward.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "a.cmd.param:0.maxspeed" > /dev/ttyUSB0
+echo "b.cmd.param:0.maxspeed" > /dev/ttyUSB0
+echo "c.cmd.param:0.maxspeed" > /dev/ttyUSB0
+echo "d.cmd.param:0.maxspeed" > /dev/ttyUSB0
+
+
 echo "a.speed:60000" > /dev/ttyUSB0
 echo "a.turn:fwd" > /dev/ttyUSB0
 echo "b.speed:60000" > /dev/ttyUSB0
@@ -8,10 +14,10 @@ echo "c.turn:fwd" > /dev/ttyUSB0
 echo "d.speed:60000" > /dev/ttyUSB0
 echo "d.turn:fwd" > /dev/ttyUSB0
 
-echo "a.cmd.volt:0.kvaldec" > /dev/ttyUSB0
-echo "b.cmd.volt:0.kvaldec" > /dev/ttyUSB0
-echo "c.cmd.volt:0.kvaldec" > /dev/ttyUSB0
-echo "d.cmd.volt:0.kvaldec" > /dev/ttyUSB0
+echo "a.cmd.param:0.kvaldec" > /dev/ttyUSB0
+echo "b.cmd.param:0.kvaldec" > /dev/ttyUSB0
+echo "c.cmd.param:0.kvaldec" > /dev/ttyUSB0
+echo "d.cmd.param:0.kvaldec" > /dev/ttyUSB0
 
 echo "run" > /dev/ttyUSB0
 
