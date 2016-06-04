9cf30423f04ad4447fcbb7f72c1e5c8a3d3dd443
Ma Eikei <maeikei@gmail.com>
2016-06-04 21:38:47 +0900

    add commands

---
+++ shell/subdrift.lock.left.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "a.cmd.volt:64.kvalrun" > /dev/ttyUSB0
+echo "b.cmd.volt:64.kvalrun" > /dev/ttyUSB0
+echo "c.cmd.volt:64.kvalrun" > /dev/ttyUSB0
+echo "d.cmd.volt:64.kvalrun" > /dev/ttyUSB0
+echo "a.speed:30000" > /dev/ttyUSB0
+echo "a.turn:rev" > /dev/ttyUSB0
+echo "c.speed:30000" > /dev/ttyUSB0
+echo "c.turn:rev" > /dev/ttyUSB0
+echo "run" > /dev/ttyUSB0
