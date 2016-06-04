44c8107fc4855ff6adeab950161eac11d159e334
ma yingkui <maeikei@gmail.com>
2016-06-04 21:43:29 +0900

    Update subdrift.left.sh

--- shell/subdrift.left.sh
+++ shell/subdrift.left.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "a.cmd.volt:64.kvalrun" > /dev/ttyUSB0
-echo "b.cmd.volt:64.kvalrun" > /dev/ttyUSB0
-echo "c.cmd.volt:64.kvalrun" > /dev/ttyUSB0
-echo "d.cmd.volt:64.kvalrun" > /dev/ttyUSB0
+echo "a.cmd.param:64.kvalrun" > /dev/ttyUSB0
+echo "b.cmd.param:64.kvalrun" > /dev/ttyUSB0
+echo "c.cmd.param:64.kvalrun" > /dev/ttyUSB0
+echo "d.cmd.param:64.kvalrun" > /dev/ttyUSB0
 echo "a.speed:30000" > /dev/ttyUSB0
 echo "a.turn:rev" > /dev/ttyUSB0
 echo "c.speed:30000" > /dev/ttyUSB0
