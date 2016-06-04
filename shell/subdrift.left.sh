8a80d86d7219a8f38d9dee260d8c37e34937a795
ma yingkui <maeikei@gmail.com>
2016-06-04 17:05:27 +0900

    Update subdrift.left.sh

--- shell/subdrift.left.sh
+++ shell/subdrift.left.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-./drift.forward.sh
-sleep 0.8
-./drift.forward.sh
-sleep 0.5
-
 echo "a.cmd.volt:64.kvalrun" > /dev/ttyUSB0
 echo "b.cmd.volt:64.kvalrun" > /dev/ttyUSB0
 echo "c.cmd.volt:64.kvalrun" > /dev/ttyUSB0
