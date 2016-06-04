2175db3be443e3a57afb6a0797d7fdd86123fe0f
ma yingkui <maeikei@gmail.com>
2016-06-04 21:36:58 +0900

    Update subdrift.stop.sh

--- shell/subdrift.stop.sh
+++ shell/subdrift.stop.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "a.cmd.param:64.maxspeed" > /dev/ttyUSB0
+echo "b.cmd.param:64.maxspeed" > /dev/ttyUSB0
+echo "c.cmd.param:64.maxspeed" > /dev/ttyUSB0
+echo "d.cmd.param:64.maxspeed" > /dev/ttyUSB0
+
 echo "a.cmd.volt:48.kvalrun" > /dev/ttyUSB0
 echo "b.cmd.volt:48.kvalrun" > /dev/ttyUSB0
 echo "c.cmd.volt:48.kvalrun" > /dev/ttyUSB0
