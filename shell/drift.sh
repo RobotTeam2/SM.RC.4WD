f3143e6199e59b68cc79df9e541a55944801bad2
ma yingkui <maeikei@gmail.com>
2016-06-04 17:05:01 +0900

    Update drift.sh

--- shell/drift.sh
+++ shell/drift.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
-./drift.forward.sh
+./subdrift.forward.sh
 sleep 0.8
-./drift.forward.sh
+./subdrift.forward.sh
 sleep 0.5
+./subdrift.left.sh
 
-echo "a.cmd.volt:64.kvalrun" > /dev/ttyUSB0
-echo "b.cmd.volt:64.kvalrun" > /dev/ttyUSB0
-echo "c.cmd.volt:64.kvalrun" > /dev/ttyUSB0
-echo "d.cmd.volt:64.kvalrun" > /dev/ttyUSB0
-echo "a.speed:30000" > /dev/ttyUSB0
-echo "a.turn:rev" > /dev/ttyUSB0
-echo "c.speed:30000" > /dev/ttyUSB0
-echo "c.turn:rev" > /dev/ttyUSB0
-echo "run" > /dev/ttyUSB0
-
-sleep 1
-echo "a.cmd.volt:48.kvalrun" > /dev/ttyUSB0
-echo "b.cmd.volt:48.kvalrun" > /dev/ttyUSB0
-echo "c.cmd.volt:48.kvalrun" > /dev/ttyUSB0
-echo "d.cmd.volt:48.kvalrun" > /dev/ttyUSB0
