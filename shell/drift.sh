9d748d6e8b4af25c0724d56317f475589b16f658
ma yingkui <maeikei@gmail.com>
2016-06-04 21:53:13 +0900

    Update drift.sh

--- shell/drift.sh
+++ shell/drift.sh
@@ -4,6 +4,17 @@ sleep 0.5
 ./subdrift.lock.left.sh
 sleep 0.2
 ./subdrift.free.left.sh
+sleep 0.2
+./subdrift.lock.left.sh
+sleep 0.2
+./subdrift.free.left.sh
+sleep 0.2
+./subdrift.lock.left.sh
+sleep 0.2
+./subdrift.free.left.sh
+
+
+
 sleep 1
 ./subdrift.stop.sh
 
