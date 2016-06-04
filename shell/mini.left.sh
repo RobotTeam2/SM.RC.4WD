1b300d6e6f6bf952e6498aa21da2fb124ff81a1a
ma yingkui <maeikei@gmail.com>
2016-06-04 22:29:02 +0900

    Update mini.left.sh

--- shell/mini.left.sh
+++ shell/mini.left.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "a.speed:10000" > /dev/ttyUSB0
 echo "a.turn:fwd" > /dev/ttyUSB0
-
-echo "b.cmd.param:64.kvalhold" > /dev/ttyUSB0
-echo "b.cmd.softstop" > /dev/ttyUSB0
-
+echo "b.speed:10000" > /dev/ttyUSB0
+echo "b.turn:rev" > /dev/ttyUSB0
 echo "c.speed:10000" > /dev/ttyUSB0
 echo "c.turn:fwd" > /dev/ttyUSB0
 echo "d.speed:10000" > /dev/ttyUSB0
