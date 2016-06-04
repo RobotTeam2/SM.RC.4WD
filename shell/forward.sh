5d14eb0cb41f17fd28bbdfdaef39cf5d7a370110
ma yingkui <maeikei@gmail.com>
2016-06-04 22:03:18 +0900

    Update forward.sh

--- shell/forward.sh
+++ shell/forward.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "a.speed:50000" > /dev/ttyUSB0
+echo "a.speed:40000" > /dev/ttyUSB0
 echo "a.turn:fwd" > /dev/ttyUSB0
-echo "b.speed:50000" > /dev/ttyUSB0
+echo "b.speed:40000" > /dev/ttyUSB0
 echo "b.turn:fwd" > /dev/ttyUSB0
-echo "c.speed:50000" > /dev/ttyUSB0
+echo "c.speed:40000" > /dev/ttyUSB0
 echo "c.turn:fwd" > /dev/ttyUSB0
-echo "d.speed:50000" > /dev/ttyUSB0
+echo "d.speed:40000" > /dev/ttyUSB0
 echo "d.turn:fwd" > /dev/ttyUSB0
 echo "run" > /dev/ttyUSB0
 
