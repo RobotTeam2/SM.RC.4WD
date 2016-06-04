af7a070b51950016842fa28adaa8a7e4df4e60ed
Ma Eikei <maeikei@gmail.com>
2016-06-04 22:27:45 +0900

    add commands

---
+++ shell/mini.left.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "a.speed:10000" > /dev/ttyUSB0
+echo "a.turn:fwd" > /dev/ttyUSB0
+
+echo "b.cmd.param:64.kvalhold" > /dev/ttyUSB0
+echo "b.cmd.softstop" > /dev/ttyUSB0
+
+echo "c.speed:10000" > /dev/ttyUSB0
+echo "c.turn:fwd" > /dev/ttyUSB0
+echo "d.speed:10000" > /dev/ttyUSB0
+echo "d.turn:rev" > /dev/ttyUSB0
+echo "run" > /dev/ttyUSB0
