a1ca370de687df29f2773607dfaff6dda2d952ac
ma yingkui <maeikei@gmail.com>
2016-06-04 17:07:03 +0900

    Update subdrift.left.sh

--- shell/subdrift.left.sh
+++ shell/subdrift.left.sh
@@ -8,9 +8,3 @@ echo "a.turn:rev" > /dev/ttyUSB0
 echo "c.speed:30000" > /dev/ttyUSB0
 echo "c.turn:rev" > /dev/ttyUSB0
 echo "run" > /dev/ttyUSB0
-
-sleep 1
-echo "a.cmd.volt:48.kvalrun" > /dev/ttyUSB0
-echo "b.cmd.volt:48.kvalrun" > /dev/ttyUSB0
-echo "c.cmd.volt:48.kvalrun" > /dev/ttyUSB0
-echo "d.cmd.volt:48.kvalrun" > /dev/ttyUSB0
