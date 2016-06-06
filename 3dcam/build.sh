4877665b3ca5b18efb4d6fb0d589a1181eef8b94
ma yingkui <maeikei@gmail.com>
2016-06-07 00:14:58 +0900

    Update build.sh

--- 3dcam/build.sh
+++ 3dcam/build.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
 sudo apt-get install cmake libopencv-dev libusb-1.0-dev
+
+rm -rf libuvc
+# build libuvc
+git clone https://github.com/ktossell/libuvc
+cd libuvc
+mkdir build
+cd build
+cmake ..
+make && sudo make install
+
+cd ../../
+mkdir object
+cd object
+cmake ..
+make
 
