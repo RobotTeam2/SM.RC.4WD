23e0b0e42fa41919a2b79cefe1f2f909dd74fe36
Ma Eikei <maeikei@gmail.com>
2016-06-07 22:28:46 +0900

    add commands

---
+++ shell/subdrift.big.left.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "a.speed:20000" > /dev/ttyUSB0
+echo "a.turn:fwd" > /dev/ttyUSB0
+echo "b.speed:10000" > /dev/ttyUSB0
+echo "b.turn:fwd" > /dev/ttyUSB0
+echo "c.speed:20000" > /dev/ttyUSB0
+echo "c.turn:fwd" > /dev/ttyUSB0
+echo "d.speed:10000" > /dev/ttyUSB0
+echo "d.turn:fwd" > /dev/ttyUSB0
+echo "run" > /dev/ttyUSB0
