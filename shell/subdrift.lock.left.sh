2a0df681cf8a003d826bd4d3cd862e01eba55e9b
ma yingkui <maeikei@gmail.com>
2016-06-07 22:44:45 +0900

    Update subdrift.lock.left.sh

--- shell/subdrift.lock.left.sh
+++ shell/subdrift.lock.left.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "a.cmd.param:64.kvalrun" > /dev/ttyUSB0
+echo "a.cmd.param:64.kvalhold" > /dev/ttyUSB0
 echo "b.cmd.param:64.kvalrun" > /dev/ttyUSB0
 echo "c.cmd.param:64.kvalrun" > /dev/ttyUSB0
 echo "d.cmd.param:64.kvalrun" > /dev/ttyUSB0
 
 
-echo "a.speed:10000" > /dev/ttyUSB0
-echo "a.turn:fwd" > /dev/ttyUSB0
+#echo "a.speed:10000" > /dev/ttyUSB0
+#echo "a.turn:fwd" > /dev/ttyUSB0
+echo "d.cmd.hardstop" > /dev/ttyUSB0
+
 echo "b.speed:1000" > /dev/ttyUSB0
 echo "b.turn:fwd" > /dev/ttyUSB0
 echo "c.speed:40000" > /dev/ttyUSB0
