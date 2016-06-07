b7982f0d7fbc0574ceb280ed325aafd9f74143b2
ma yingkui <maeikei@gmail.com>
2016-06-07 22:31:52 +0900

    Update subdrift.big.left.sh

--- shell/subdrift.big.left.sh
+++ shell/subdrift.big.left.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "a.speed:20000" > /dev/ttyUSB0
+echo "a.speed:40000" > /dev/ttyUSB0
 echo "a.turn:fwd" > /dev/ttyUSB0
 echo "b.speed:10000" > /dev/ttyUSB0
 echo "b.turn:fwd" > /dev/ttyUSB0
-echo "c.speed:20000" > /dev/ttyUSB0
+echo "c.speed:40000" > /dev/ttyUSB0
 echo "c.turn:fwd" > /dev/ttyUSB0
 echo "d.speed:10000" > /dev/ttyUSB0
 echo "d.turn:fwd" > /dev/ttyUSB0
