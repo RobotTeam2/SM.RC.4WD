b199f55c88bb8b51f8c02bef4be7568e973bdab9
ma yingkui <maeikei@gmail.com>
2016-06-07 22:54:49 +0900

    Update subdrift.big.left.sh

--- shell/subdrift.big.left.sh
+++ shell/subdrift.big.left.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-
-echo "a.cmd.param:200.maxspeed" > /dev/ttyUSB0
-echo "b.cmd.param:200.maxspeed" > /dev/ttyUSB0
-echo "c.cmd.param:200.maxspeed" > /dev/ttyUSB0
-echo "d.cmd.param:200.maxspeed" > /dev/ttyUSB0
-
 echo "a.speed:30000" > /dev/ttyUSB0
 echo "a.turn:fwd" > /dev/ttyUSB0
-echo "b.speed:5000" > /dev/ttyUSB0
+echo "b.speed:10000" > /dev/ttyUSB0
 echo "b.turn:fwd" > /dev/ttyUSB0
 echo "c.speed:30000" > /dev/ttyUSB0
 echo "c.turn:fwd" > /dev/ttyUSB0
-echo "d.speed:5000" > /dev/ttyUSB0
+echo "d.speed:10000" > /dev/ttyUSB0
 echo "d.turn:fwd" > /dev/ttyUSB0
 echo "run" > /dev/ttyUSB0
