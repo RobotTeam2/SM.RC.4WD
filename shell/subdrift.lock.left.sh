829ff01c5444dd65ba76eeed1b090e64ae26ca3f
ma yingkui <maeikei@gmail.com>
2016-06-07 23:00:15 +0900

    Update subdrift.lock.left.sh

--- shell/subdrift.lock.left.sh
+++ shell/subdrift.lock.left.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-echo "a.cmd.param:64.kvalhold" > /dev/ttyUSB0
+echo "a.cmd.param:64.kvalrun" > /dev/ttyUSB0
+echo "a.speed:100" > /dev/ttyUSB0
+echo "a.turn:fwd" > /dev/ttyUSB0
+#echo "a.cmd.param:64.kvalhold" > /dev/ttyUSB0
+#echo "d.cmd.hardstop" > /dev/ttyUSB0
+
 echo "b.cmd.param:64.kvalrun" > /dev/ttyUSB0
-echo "c.cmd.param:64.kvalrun" > /dev/ttyUSB0
-echo "d.cmd.param:64.kvalrun" > /dev/ttyUSB0
-
-
-#echo "a.speed:10000" > /dev/ttyUSB0
-#echo "a.turn:fwd" > /dev/ttyUSB0
-echo "d.cmd.hardstop" > /dev/ttyUSB0
-
-echo "b.speed:20000" > /dev/ttyUSB0
+echo "b.speed:30000" > /dev/ttyUSB0
 echo "b.turn:fwd" > /dev/ttyUSB0
-echo "c.speed:40000" > /dev/ttyUSB0
+
+echo "c.cmd.param:64.kvalrun" > /dev/ttyUSB0
+echo "c.speed:30000" > /dev/ttyUSB0
 echo "c.turn:fwd" > /dev/ttyUSB0
-echo "d.speed:40000" > /dev/ttyUSB0
-echo "d.turn:fwd" > /dev/ttyUSB0
+
+echo "d.cmd.param:64.kvalrun" > /dev/ttyUSB0
+echo "d.speed:30000" > /dev/ttyUSB0
+echo "d.turn:rev" > /dev/ttyUSB0
 echo "run" > /dev/ttyUSB0
