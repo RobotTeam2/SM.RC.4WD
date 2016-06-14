ee68840dd5ec03c2b285f490b4d442bfb0a388b1
ma yingkui <maeikei@gmail.com>
2016-06-15 00:37:21 +0900

    Update main.c

--- MPU6050/main.c
+++ MPU6050/main.c
@@ -7,6 +7,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #include "MotionSensor.h"
 
@@ -17,15 +20,15 @@ int main() {
 	ms_open();
 
 	const char * gMpufifo = "/tmp/mpu.6050.fifo";
-	mkfifo(gMpufifo, 0777);
-	int fd = open(gMpufifo, O_WRONLY|O_NONBLOCK);
+	int fd = socket( AF_LOCAL, SOCK_DGRAM, 0 );
+	sockaddr_un addr = SOCKADDR_UN_INIT( AF_LOCAL, gMpufifo );
 	do{
 		ms_update();
 		printf("yaw = %5.4f\tpitch = %5.4f\troll = %5.4f\ttemperature = %5.4f\n",
 		 ypr[YAW], ypr[PITCH],ypr[ROLL],temp);
 		 char buff[256];
 		 sprintf(buff, "{yaw:%f,pitch:%f,roll:%f,temperature:%f}\n", ypr[YAW], ypr[PITCH],ypr[ROLL],temp);
-		 write(fd,buff,strlen(buff));
+		 sendto(fd, buff, strlen(buff), 0, (sockadrr*)&addr, sizeof(addr) );
 #if 0
 		printf("yaw = %5.4f\tpitch = %4.5f\troll = %5.4f\ttemperature = %5.4f\n",
 		 gyro[YAW], gyro[PITCH],
@@ -34,7 +37,5 @@ int main() {
 		delay_ms(1);
 	}while(1);
 	close(fd);
-	unlink(gMpufifo);
-
 	return 0;
 }
