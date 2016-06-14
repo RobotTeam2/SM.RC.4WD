9d6d134ab09d6b4376fa4b5335b652f012a555cc
ma yingkui <maeikei@gmail.com>
2016-06-15 00:10:55 +0900

    Update turn.c

--- MPU6050/turn.c
+++ MPU6050/turn.c
@@ -13,13 +13,16 @@ int main()
 
     /* open, read, and display the message from the FIFO */
     fd = open(gMpufifo, O_RDONLY|O_NONBLOCK);
-    int ret = read(fd, buf, MAX_BUF);
-    if(ret>0){
-        printf("Received: %s\n", buf);
-    } else if (ret==0){
-        printf("noting \n");
-    } else {
-        printf("ret=<%d>\n",ret);
+    int degree = 90;
+    while(degree >0) {
+        int ret = read(fd, buf, MAX_BUF);
+        if(ret>0){
+            printf("Received: %s\n", buf);
+        } else if (ret==0){
+            printf("noting \n");
+        } else {
+            printf("ret=<%d>\n",ret);
+        }
     }
     close(fd);
 
