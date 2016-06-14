a8365728751856a599555405b58e8e69d6d3ccb6
ma yingkui <maeikei@gmail.com>
2016-06-15 00:05:59 +0900

    Update turn.c

--- MPU6050/turn.c
+++ MPU6050/turn.c
@@ -12,7 +12,7 @@ int main()
     char buf[MAX_BUF];
 
     /* open, read, and display the message from the FIFO */
-    fd = open(gMpufifo, O_RDONLY);
+    fd = open(gMpufifo, O_RDONLY|O_NONBLOCK);
     read(fd, buf, MAX_BUF);
     printf("Received: %s\n", buf);
     close(fd);
