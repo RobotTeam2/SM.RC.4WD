40d3bdad27338781bb139acde8b00de948c2389f
ma yingkui <maeikei@gmail.com>
2016-06-15 00:57:31 +0900

    Update turn.c

--- MPU6050/turn.c
+++ MPU6050/turn.c
@@ -2,20 +2,27 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <sys/socket.h> 
+#include <sys/un.h> 
+ 
 #define MAX_BUF 1024
 
 int main()
 {
-    int fd;
-    const char * gMpufifo = "/tmp/mpu.6050.fifo";
+    int fd = socket( AF_LOCAL, SOCK_DGRAM, 0 );
+	
+	struct sockaddr_un addr;
+	bzero( &addr, sizeof(addr) );
+	addr.sun_family = AF_LOCAL;
+	strcpy( addr.sun_path, "/tmp/mpu.6050.unix.domain" );
+	
     char buf[MAX_BUF];
 
     /* open, read, and display the message from the FIFO */
     fd = open(gMpufifo, O_RDONLY|O_NONBLOCK);
     int degree = 90;
     while(degree >0) {
-        int ret = read(fd, buf, MAX_BUF);
+        int ret = ::recvfrom(fd, buf, MAX_BUF, 0, (struct sockaddr *)addr, sizeof(addr));
         if(ret>0){
             printf("Received: %s\n", buf);
         } else if (ret==0){
