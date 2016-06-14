8c2d8627c88ae6b1153c70a370a3d740360f8880
ma yingkui <maeikei@gmail.com>
2016-06-15 00:46:28 +0900

    Update main.c

--- MPU6050/main.c
+++ MPU6050/main.c
@@ -22,16 +22,19 @@
 int main() {
 	ms_open();
 
-	const char * gMpufifo = "/tmp/mpu.6050.fifo";
 	int fd = socket( AF_LOCAL, SOCK_DGRAM, 0 );
-	sockaddr_un addr = SOCKADDR_UN_INIT( AF_LOCAL, gMpufifo );
+	
+	struct sockaddr_un addr;
+	bzero( &addr, sizeof(addr) );
+	addr.sun_family = AF_LOCAL;
+	strcpy( addr.sun_path, "/tmp/mpu.6050.unix.domain" );
 	do{
 		ms_update();
 		printf("yaw = %5.4f\tpitch = %5.4f\troll = %5.4f\ttemperature = %5.4f\n",
 		 ypr[YAW], ypr[PITCH],ypr[ROLL],temp);
 		 char buff[256];
 		 sprintf(buff, "{yaw:%f,pitch:%f,roll:%f,temperature:%f}\n", ypr[YAW], ypr[PITCH],ypr[ROLL],temp);
-		 sendto(fd, buff, strlen(buff), 0, (sockadrr*)&addr, sizeof(addr) );
+		 sendto(fd, buff, strlen(buff), 0, (struct sockadrr *)&addr, sizeof(addr) );
 #if 0
 		printf("yaw = %5.4f\tpitch = %4.5f\troll = %5.4f\ttemperature = %5.4f\n",
 		 gyro[YAW], gyro[PITCH],
