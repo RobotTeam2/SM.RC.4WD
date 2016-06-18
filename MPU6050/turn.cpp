2531de729e5056fb1e42937b4d3618d15db6f8bb
ma yingkui <maeikei@gmail.com>
2016-06-18 14:28:39 +0900

    Update turn.cpp

--- MPU6050/turn.cpp
+++ MPU6050/turn.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 const static double PI = 3.14159265;
 const static int iConstRound = 60;
-const static double iConstRoundLimit = sin(iConstRound*pi/180.0);
+const static double iConstRoundLimit = sin(iConstRound*PI/180.0);
 
 static int fd = -1;
 #include <signal.h>
