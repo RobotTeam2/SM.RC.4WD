96710fbb68b5ec9f274fb7cd4febb9da21c0c90e
Ma Eikei <maeikei@gmail.com>
2016-06-18 04:19:32 +0000

    add mpu6050

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+HOME=`dirname $0`/..
+echo $HOME
+
 stty -F /dev/ttyUSB0 115200 \
 -iuclc -ixany -imaxbel -iutf8 -opost -olcuc -ocrnl -onlcr \
 -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 -isig \
@@ -8,3 +11,4 @@ stty -F /dev/ttyUSB0 raw
 stty -F /dev/ttyUSB0 -echo -echoe -echok
 stty -F /dev/ttyUSB0
 cat /dev/ttyUSB0 &
+$HOME/MPU6050/mpu6050.service &
