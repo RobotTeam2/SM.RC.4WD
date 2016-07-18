933e334e3b4140661d8dd55fca30a46ce52a8b28
ma yingkui <maeikei@gmail.com>
2016-07-18 17:10:28 +0900

    remote rc

---
+++ RC/html/js/main.js
@@ -0,0 +1,13 @@
+
+
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  $('#dir-top').click(function(){
+    console.log( "dir-top" );
+  });
+  $('#dir-left').click(function(){
+    console.log( "dir-left" );
+  });
+});
+
