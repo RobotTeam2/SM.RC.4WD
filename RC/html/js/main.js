989e27ef6c80649dff38bd2047e13d7f118dcf91
Wator Vapor <watorvapor@gmail.com>
2016-07-18 17:48:16 +0900

    Update main.js

--- RC/html/js/main.js
+++ RC/html/js/main.js
@@ -1,14 +1,24 @@
-
-
-
 $( document ).ready(function() {
   console.log( "ready!" );
   $('#dir-top').click(function(){
     console.log( "dir-top" );
     RC4WD.top();
   });
+  $('#dir-buttom').click(function(){
+    console.log( "dir-buttom" );
+    RC4WD.buttom();
+  });
   $('#dir-left').click(function(){
     console.log( "dir-left" );
+    RC4WD.left();
+  });
+  $('#dir-right').click(function(){
+    console.log( "dir-right" );
+    RC4WD.right();
+  });
+  $('#dir-center').click(function(){
+    console.log( "dir-center" );
+    RC4WD.center();
   });
 });
 
