c274ef2617360ae8c95caf062bddfa9037b3ba7b
ma yingkui <maeikei@gmail.com>
2016-07-18 17:45:15 +0900

    remote rc

--- RC/html/js/main.js
+++ RC/html/js/main.js
@@ -5,6 +5,7 @@ $( document ).ready(function() {
   console.log( "ready!" );
   $('#dir-top').click(function(){
     console.log( "dir-top" );
+    RC4WD.top();
   });
   $('#dir-left').click(function(){
     console.log( "dir-left" );
