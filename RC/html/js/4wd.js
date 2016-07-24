1a9c6240ff51f371c22d2f44d98641a73556dd0b
ma yingkui <maeikei@gmail.com>
2016-07-24 18:25:00 +0900

    remote rc

--- RC/html/js/4wd.js
+++ RC/html/js/4wd.js
@@ -1,1 +1,17 @@
 var RC4WD = RC4WD || {};
+
+RC4WD.top = function () {
+    console.log('top');
+}
+RC4WD.buttom = function () {
+    console.log('buttom');
+}
+RC4WD.left = function () {
+    console.log('left');
+}
+RC4WD.right = function () {
+    console.log('right');
+}
+RC4WD.stop = function () {
+    console.log('stop');
+}
