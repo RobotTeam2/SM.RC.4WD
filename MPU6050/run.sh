bbeb9cfb644d54a7e6d1ad7eb5dce50af7b2c84b
Wator Vapor <watorvapor@gmail.com>
2016-07-30 17:20:40 +0900

    Update run.sh

--- MPU6050/run.sh
+++ MPU6050/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd /home/ma/workspace/SM.RC.4WD/MPU6050/ &&  ./mpu6050.service >/tmp/4wd.mpu
+#cd /home/ma/workspace/SM.RC.4WD/MPU6050/ &&  ./mpu6050.service >/tmp/4wd.mpu
+cd /home/ma/workspace/SM.RC.4WD/MPU6050/ &&  ./mpu6050.service >/dev/null
