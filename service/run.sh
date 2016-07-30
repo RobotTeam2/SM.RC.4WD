8658e22080870240ab093d2ac130d43bb9840cd6
Wator Vapor <watorvapor@gmail.com>
2016-07-30 17:20:09 +0900

    Update run.sh

--- service/run.sh
+++ service/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd /home/ma/workspace/SM.RC.4WD/service && ./sm.rc.4wd.cmd>/tmp/4wd.car
+#cd /home/ma/workspace/SM.RC.4WD/service && ./sm.rc.4wd.cmd>/tmp/4wd.car
+cd /home/ma/workspace/SM.RC.4WD/service && ./sm.rc.4wd.cmd > /dev/null
