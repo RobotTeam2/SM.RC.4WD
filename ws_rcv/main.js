590a0569d85ceb40b1a74a98b625fe40b9d937d5
Wator Vapor <watorvapor@gmail.com>
2016-08-29 22:10:12 +0900

    Update main.js

--- ws_rcv/main.js
+++ ws_rcv/main.js
@@ -8,8 +8,8 @@ var server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+server.listen(18001, function() {
+    console.log((new Date()) + ' Server is listening on port 18001');
 });
 
 wsServer = new WebSocketServer({
