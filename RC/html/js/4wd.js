b5a6a14569c03801e67e7486bfa6d97f6d79f472
Wator Vapor <watorvapor@gmail.com>
2016-08-29 22:11:38 +0900

    Update 4wd.js

--- RC/html/js/4wd.js
+++ RC/html/js/4wd.js
@@ -1,5 +1,5 @@
 var RC4WD = RC4WD || {};
-RC4WD.ws = RC4WD.ws || new WebSocket(location.host+'/ws');
+RC4WD.ws = RC4WD.ws || new WebSocket('ws://'+location.host+'/ws');
 RC4WD.top = function () {
     console.log('top');
     if(BLE) {
