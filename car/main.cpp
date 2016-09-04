6b1a72d73b39e2da6ee5eba2893da927ff2276b7
Wator Vapor <watorvapor@gmail.com>
2016-09-04 22:25:55 +0900

    Update main.cpp

--- car/main.cpp
+++ car/main.cpp
@@ -3,14 +3,14 @@
 #include <thread>
 
 #define DUMP_VAR(x) std::cout << #x << "=<" << x <<">" << std::endl;
-extern void ble_upd_main(void);
+extern void ble_ws_upd_main(void);
 extern void mpu_udp_main(void);
 extern void car_uart_main(void);
 
 int main(int argc, char * argv[])
 {     
     DUMP_VAR(argc);
-    std::thread ble_udp_thd(ble_upd_main);
+    std::thread ble_udp_thd(ble_ws_upd_main);
     std::thread mpu_udp_thd(mpu_udp_main);
     std::thread car_uart_thd(car_uart_main);
     
